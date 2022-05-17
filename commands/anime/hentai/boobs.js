5e3dc11b7a7fcd9c1b5566bb513a36b9fd6bbfdb
Aurelicos <96830132+Aurelicos@users.noreply.github.com>
2022-05-17 10:21:12 +0200

    Add files via upload

---
+++ commands/anime/hentai/boobs.js
@@ -0,0 +1,30 @@
+module.exports = {
+    name: 'boobs',
+    description: "command for sending boobs images/gifs",
+    execute(message, args) {
+        if (!message.channel.nsfw) { message.channel.send("This command can only be used in nsfw channels."); return; }
+        if (message.member.roles.cache.find(r => r.name === "18+")) {
+            const command = args.shift();
+            const API = require('anime-images-api')
+            const images_api = new API()
+            if (command === undefined) {
+                images_api.nsfw.boobs().then(response => {
+                    message.channel.send(response.image)
+                })
+            }
+            else if (command === 'imagebomb') {
+                for (let i = 0; i < 5; i++) {
+                    images_api.nsfw.boobs().then(response => {
+                        message.channel.send(response.image)
+                    })
+                }
+            }
+            else {
+                message.channel.send("Invalid option! Try \`$help boobs\`")
+            }
+        }
+        else {
+            message.channel.send("***Warning***, You need ***\`18+\`*** role to send these images.")
+        }
+    }
+}
